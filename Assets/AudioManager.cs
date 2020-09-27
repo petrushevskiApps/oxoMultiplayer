0d2da5c89ac3860fb16d6005d90c4a7b2bec8487
Aleksandar Petrushevski <petrushevskiapps@gmail.com>
2020-09-27 17:01:16 +0200

    Added Audio Manager and Background Music, PlayerDataController improved and connect with AudioManager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource backgroundMusicSource;
+    [SerializeField] private AudioSource sfxMusicSource;
+
+    public static AudioManager Instance;
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
+        PlayerDataController.backgroundMusicStatusChange.AddListener(OnBgMusicStatusChange);
+        PlayerDataController.sfxStatusChange.AddListener(OnSfxStatusChange);
+    }
+
+    private void OnDestroy()
+    {
+        PlayerDataController.backgroundMusicStatusChange.RemoveListener(OnBgMusicStatusChange);
+        PlayerDataController.sfxStatusChange.RemoveListener(OnSfxStatusChange);
+    }
+
+    private void OnEnable()
+    {
+        OnSfxStatusChange(PlayerDataController.SfxStatus);
+        OnBgMusicStatusChange(PlayerDataController.BackgroundMusicStatus);
+    }
+
+    private void OnSfxStatusChange(bool status)
+    {
+        sfxMusicSource.enabled = status;
+    }
+
+    private void OnBgMusicStatusChange(bool status)
+    {
+        backgroundMusicSource.enabled = status;
+    }
+}
--- Assets/Scripts/PlayerDataController.cs
+++ Assets/Scripts/PlayerDataController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerDataController : MonoBehaviour
 {
@@ -13,39 +14,79 @@ public class PlayerDataController : MonoBehaviour
     private const string KEY_SFX_STATUS = "sfxStatus";
     private const string KEY_VIBRATION_STATUS = "vibrationStatus";
 
+    public static UnityBoolEvent backgroundMusicStatusChange = new UnityBoolEvent();
+    public static UnityBoolEvent sfxStatusChange = new UnityBoolEvent();
+    public static UnityBoolEvent vibrationStatusChange = new UnityBoolEvent();
+    
+    public static UnityIntegerEvent playedGamesChange = new UnityIntegerEvent();
+    public static UnityIntegerEvent wonGamesChange = new UnityIntegerEvent();
+    public static UnityIntegerEvent lostGamesChange = new UnityIntegerEvent();
+
+   
     public static string Username
     {
         get => PlayerPrefs.GetString(KEY_USERNAME, string.Empty);
         set => PlayerPrefs.SetString(KEY_USERNAME, value);
     }
-    public static int PlayedGames
+    public static int PlayedGames => PlayerPrefs.GetInt(KEY_PLAYED_GAMES, 0);
+
+    public static void IncreasePlayedGames()
     {
-        get => PlayerPrefs.GetInt(KEY_PLAYED_GAMES, -1);
-        set => PlayerPrefs.SetInt(KEY_PLAYED_GAMES, value);
+        int playedGames = PlayerPrefs.GetInt(KEY_PLAYED_GAMES, 0) + 1;
+        PlayerPrefs.SetInt(KEY_PLAYED_GAMES, playedGames);
+        playedGamesChange.Invoke(playedGames);
     }
-    public static int WonGames
+    
+    public static int WonGames => PlayerPrefs.GetInt(KEY_WON_GAMES, 0);
+
+    public static void IncreaseWonGames()
     {
-        get => PlayerPrefs.GetInt(KEY_WON_GAMES, 0);
-        set => PlayerPrefs.SetInt(KEY_WON_GAMES, value);
+        int wonGames = PlayerPrefs.GetInt(KEY_WON_GAMES, 0) + 1;
+        PlayerPrefs.SetInt(KEY_WON_GAMES, wonGames);
+        wonGamesChange.Invoke(wonGames);
     }
-    public static int LostGames
+    
+    public static int LostGames => PlayerPrefs.GetInt(KEY_LOST_GAMES, 0);
+
+    public static void IncreaseLostGames()
     {
-        get => PlayerPrefs.GetInt(KEY_LOST_GAMES, 0);
-        set => PlayerPrefs.SetInt(KEY_LOST_GAMES, value);
+        int lostGames = PlayerPrefs.GetInt(KEY_LOST_GAMES, 0) + 1;
+        PlayerPrefs.SetInt(KEY_LOST_GAMES, lostGames);
+        lostGamesChange.Invoke(lostGames);
     }
+    
     public static bool BackgroundMusicStatus
     {
         get => PlayerPrefs.GetInt(KEY_BG_MUSIC_STATUS, 1) == 1;
-        set => PlayerPrefs.SetInt(KEY_BG_MUSIC_STATUS, value ? 1 : 0);
+        set
+        {
+            PlayerPrefs.SetInt(KEY_BG_MUSIC_STATUS, value ? 1 : 0);
+            backgroundMusicStatusChange.Invoke(value);
+        }
     }
     public static bool SfxStatus
     {
         get => PlayerPrefs.GetInt(KEY_SFX_STATUS, 1) == 1;
-        set => PlayerPrefs.SetInt(KEY_SFX_STATUS, value ? 1 : 0);
+        set
+        {
+            PlayerPrefs.SetInt(KEY_SFX_STATUS, value ? 1 : 0);
+            sfxStatusChange.Invoke(value);
+        }
     }
     public static bool VibrationStatus
     {
         get => PlayerPrefs.GetInt(KEY_VIBRATION_STATUS, 1) == 1;
-        set => PlayerPrefs.SetInt(KEY_VIBRATION_STATUS, value ? 1 : 0);
+        set
+        {
+            PlayerPrefs.SetInt(KEY_VIBRATION_STATUS, value ? 1 : 0);
+            vibrationStatusChange.Invoke(value);
+        }
+    }
+
+    public class UnityBoolEvent : UnityEvent<bool>
+    {
+    }
+    public class UnityIntegerEvent : UnityEvent<int>
+    {
     }
 }
